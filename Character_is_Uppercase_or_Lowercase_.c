dde8596c6b4c24ee19e4bdde06ebc10ceafd71f1
PavaniVattikolla <147476015+PavaniVattikolla@users.noreply.github.com>
2023-10-24 19:18:58 +0530

    #include<stdio.h> int main() {     char ch;     scanf("%c",&ch);     if (ch>=67&&ch<=90)     {         printf("uppercase alphabet ");     }     else if (ch>=97&&ch<=122)     {         printf("lowercase alphabet ");     }     else     {         printf("not an alphabet ");     } }

---
+++ Character_is_Uppercase_or_Lowercase_.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{   
+    char ch;
+    scanf("%c",&ch);
+    if (ch>=67&&ch<=90)
+    {
+        printf("uppercase alphabet
+");
+    }
+    else if (ch>=97&&ch<=122)
+    {
+        printf("lowercase alphabet
+");
+    }
+    else
+    {
+        printf("not an alphabet
+");
+    }
+}
