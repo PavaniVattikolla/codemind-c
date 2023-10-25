a6bec5d31de4235b80fc060e87b015d384ef5124
PavaniVattikolla <147476015+PavaniVattikolla@users.noreply.github.com>
2023-10-25 20:10:13 +0530

    #include<stdio.h> int main() {     int x,y,t;     scanf("%d%d",&x,&y);     t=y*30;     if (x>=t)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Expenditure.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{   
+    int x,y,t;
+    scanf("%d%d",&x,&y);
+    t=y*30;
+    if (x>=t)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
+    
