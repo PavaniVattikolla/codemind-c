f4c9d380d028c8e057d00ef1476b64ac4adc9ec7
PavaniVattikolla <147476015+PavaniVattikolla@users.noreply.github.com>
2023-10-26 19:42:55 +0530

    #include<stdio.h> int main() {     int x,y,d;     scanf("%d%d",&x,&y);     d=y-x;     if (d<=0)     {         printf("%d",d);     }     else     {         printf("%d",d);     } }

---
+++ Donation.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,d;
+    scanf("%d%d",&x,&y);
+    d=y-x;
+    if (d<=0)
+    {
+        printf("%d",d);
+    }
+    else
+    {
+        printf("%d",d);
+    }
+}
