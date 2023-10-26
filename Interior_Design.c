5ecd23271eba78a8bee1afef513853c4a840a377
PavaniVattikolla <147476015+PavaniVattikolla@users.noreply.github.com>
2023-10-26 19:58:59 +0530

    #include<stdio.h> int main() {     int x,y,a,b,z,c;     scanf("%d%d%d%d",&x,&y,&a,&b);     z=x+y;     c=a+b;     if (z<c)     {         printf("%d",z);     }     else if (z>c)     {         printf("%d",c);     }     else if (x=c)     {         printf("%d",x);     } }

---
+++ Interior_Design.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,a,b,z,c;
+    scanf("%d%d%d%d",&x,&y,&a,&b);
+    z=x+y;
+    c=a+b;
+    if (z<c)
+    {
+        printf("%d",z);
+    }
+    else if (z>c)
+    {
+        printf("%d",c);
+    }
+    else if (x=c)
+    {
+        printf("%d",x);
+    }
+}
