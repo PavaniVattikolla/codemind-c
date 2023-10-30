55e7c4abcde0a3274a16eb5852279d1c9c87ba6f
PavaniVattikolla <147476015+PavaniVattikolla@users.noreply.github.com>
2023-10-30 11:43:33 +0530

    #include<stdio.h> int main() {   int i;   for(i=1;i<=10;i++)   {     printf("%d ",i);   } }

--- 1_to_10.c
+++ 1_to_10.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int i=1;
-    while (i<=10)
-    {
-        printf("%d ",i);
-        i++;
-    }
+  int i;
+  for(i=1;i<=10;i++)
+  {
+    printf("%d ",i);
+  }
 }
